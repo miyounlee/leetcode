de2118c1417a28b98c2451e87e8c51d4e8ee5cba
Miyeon Lee <leeyun92@naver.com>
2024-02-15 21:32:39 +0900

    Time: 10 ms (91.78%), Space: 48.7 MB (41.46%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,26 +1,26 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         // 1. 그래프 구현
-        List<Pair>[] graph = new List[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            graph[i] = new ArrayList<>();
+        List<List<Pair>> graph = new ArrayList<>(n + 1);    // 제네릭타입의 일관성을 위해 graph를 배열에서 List로 수정
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
         }
         for (int[] time : times) {
             int u = time[0];
             int v = time[1];
             int w = time[2];
-            graph[u].add(new Pair(v, w));
+            graph.get(u).add(new Pair(v, w));
         }
         int[] costs = new int[n + 1];
         Arrays.fill(costs, Integer.MAX_VALUE);
 
         // 2. 다익스트라
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(p -> p.cost));
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
         pq.offer(new Pair(k, 0));
         costs[k] = 0;
         while (!pq.isEmpty()) {
             Pair current = pq.poll();
-            for (Pair pair : graph[current.node]) {
+            for (Pair pair : graph.get(current.node)) {
                 int next = pair.node;
                 int cost = pair.cost;
                 if (costs[current.node] + cost < costs[next]) {
@@ -43,12 +43,17 @@ class Solution {
     }
 }
 
-class Pair {
+class Pair implements Comparable<Pair>{
     int node;
     int cost;
 
     public Pair(int node, int cost) {
         this.node = node;
         this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Pair o) {
+        return this.cost - o.cost;
     }
 }
